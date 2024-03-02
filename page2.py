4fe8fd0dbc6d7d72a48391cd7313d527679e0b91
naweezy <nuurulazisa@gmail.com>
2024-03-01 19:30:44 -0800

    R10

--- page2.py
+++ page2.py
@@ -3,3 +3,4 @@ import streamlit as st
 def page_2():
   st.title("Halaman 2")
   st.write("Halaman untuk Menampilkan Youtube")
+  st.video("https://youtu.be/asFqpMDSPdM?si=lQHFeUMov8_Zgigs")
--- page3.py
+++ page3.py
@@ -2,4 +2,8 @@ import streamlit as st
 
 def page_3():
   st.title("Halaman 3")
-  st.write("Halaman untuk Rumus MTK")
+  st.write("Menampilkan Halaman dari File MD (MarkDown)")
+  
+  with open('nama.md', 'r') as file:
+      isi_teks = file.read()
+      st.markdown(isi_teks)
